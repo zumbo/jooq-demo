17368c75210797201c001eb0afbffc02549468ef
Marcel Zumstein <marcel@zumbo.ch>
2022-04-14 16:41:35 +0200

    Revert "Java 11 downgrade"
    This reverts commit 19a27f3a3d27c6ee18239bfe158588c5f09490f0.

--- src/main/java/ch/zumbo/jooqdemo/JooqDemoApplication.java
+++ src/main/java/ch/zumbo/jooqdemo/JooqDemoApplication.java
@@ -8,7 +8,6 @@ public class JooqDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JooqDemoApplication.class, args);
-		System.out.println("Demo-Frontend erreichbar unter: http://localhost:8080/demo/execute");
 		System.out.println("H2 Console erreichbar unter: http://localhost:8080/h2/");
 	}
 
